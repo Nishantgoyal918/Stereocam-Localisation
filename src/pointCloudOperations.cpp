fce984bd157bc283acef771bb1e365a40aeb541d
Nishantgoyal918 <nishantgoyal918@gmail.com>
2020-12-15 16:49:31 +0530

    Added point cloud operations class

---
+++ src/pointCloudOperations.cpp
@@ -0,0 +1,18 @@
+#include "pointCloudOperations.h"
+
+PointCloudOperations::PointCloudOperations(ros::NodeHandle &node) : nh(node){
+
+
+}
+
+PointCloudOperations::~PointCloudOperations(){
+
+}
+
+PointCloudOperations::transform(){
+
+}
+
+PointCloudOperations::outTreeSearch(){
+
+}
---
+++ src/pointCloudOperations.h
@@ -0,0 +1,38 @@
+#ifndef POINTCLOUDOPERATIONS_H
+#define POINTCLOUDOPERATIONS_H
+
+#include <ros/ros.h>
+
+/**
+ * Point Cloud Operations Class  
+ */
+class PointCloudOperations {
+public:
+
+    /**
+     * Default Constructor 
+     */
+    PointCloudOperations(ros::NodeHandle& node);
+
+    ~PointCloudOperations();
+
+
+private:
+
+    ros::NodeHandle& nh;
+
+
+public:
+
+    /**
+     * 
+     */
+    void transform();
+
+    /**
+     * Function to find the local map corresponding to current position
+     */
+    void outTreeSearch();
+}
+
+#endif
